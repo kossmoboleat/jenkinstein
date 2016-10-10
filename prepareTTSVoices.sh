0927f10c94926cf5fd3a0d41c329526c33ac6e09
Tim Benke <kossmoboleat@gmail.com>
2016-10-10 21:22:56 +0200

    removed unused shell script for installing spike tts voice

--- prepareTTSVoices.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-#mvn install:install-file -Dfile=lib/voice-dfki-spike-5.1.jar -DartifactId=voice-dfki-spike -Dversion=5.1 -DgroupId=local -Dpackaging=jar -DlocalRepositoryPath=repo -DpomFile=lib/pom.xml
-
-mvn deploy:deploy-file -Dfile=lib/voice-dfki-spike-5.1.jar -DartifactId=voice-dfki-spike -Dversion=5.1 -DgroupId=local -Dpackaging=jar -Durl=file:repo -DpomFile=lib/pom.xml
