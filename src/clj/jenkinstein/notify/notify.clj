d2790f19146f0e2d7de787dacbaea332005534fc
Tim Benke <kossmoboleat@gmail.com>
2016-10-10 11:17:43 +0200

    Added handling for failed builds: Say failed build name and list culprits

--- src/clj/jenkinstein/notify/notify.clj
+++ src/clj/jenkinstein/notify/notify.clj
@@ -1,9 +1,26 @@
 (ns jenkinstein.notify.notify
-  (:require [clj-http.client :as client]))
+  (:require [clj-http.client :as client]
+            [jenkinstein.business.talk :as talk]
+            [clojure.string :refer [join]]))
+
+(defn handle-failure [job-name culprits]
+  (println "a job failed!")
+  (let [culprit-names (join ", " (map :fullName culprits))]
+    (talk/talk (str "The job " job-name " failed since " culprit-names " committed."))))
+
+(defn parse-job-name [url]
+  (second (re-find #"/job/(.*?)/" url)))
 
 (defn notify [{:keys [params]}]
-  (println "someone notified us that a job completed")
-  (let [url (str (:url params) "api/json?pretty=true")]
-    (println url)
-    (println (client/get url))
+  (let [url (str (:url params) "api/json?pretty=true")
+        job-name (parse-job-name url)
+        response (client/get url {:as :json})
+        body (:body response)
+        result (:result body)
+        culprits (:culprits body)]
+    (println "This job completed: " url)
+    (println body)
+    (if (= result "FAILURE")
+      (handle-failure job-name culprits)
+      (println "job status was: " result))
     {}))
