40909e832b22ce34633d483b73685ab4cffc92e1
Tim Benke <tim.benke@tui-infotec.com>
2016-10-23 10:47:02 +0200

    Added upload form for sound files Added basic web interface to list files, sounds

--- src/clj/jenkinstein/business/files.clj
+++ src/clj/jenkinstein/business/files.clj
@@ -1,8 +1,33 @@
 (ns jenkinstein.business.files
   (:require [clojure.java.io :as io]
-            [ring.util.http-response :refer [ok]]))
+            [ring.util.http-response :refer [ok]]
+            [ring.util.response :refer [redirect file-response]])
+  (:import [java.io File FileInputStream FileOutputStream]))
+
+(def sounds-path "resources/sounds/")
 
 (defn list-files []
-  (let [curr-dir (io/file "resources/sounds/")]
-    (map #(.getName %) (filter #(.isFile %) (file-seq curr-dir)))
-    (ok)))
+  (let [curr-dir (io/file sounds-path)
+        files (map #(.getName %) (filter #(.isFile %) (file-seq curr-dir)))]
+    (ok {:files (vec files)})))
+
+(defn- file-path [path & [filename]]
+  (java.net.URLDecoder/decode
+    (str path File/separator filename)
+    "utf-8"))
+
+(defn- upload-file-to-path
+  "uploads a file to the target folder
+   when :create-path? flag is set to true then the target path will be created"
+  [path {:keys [tempfile size filename]}]
+  (try
+    (with-open [in (new FileInputStream tempfile)
+                out (new FileOutputStream (file-path path filename))]
+      (let [source (.getChannel in)
+            dest (.getChannel out)]
+        (.transferFrom dest source 0 (.size source))
+        (.flush out)))))
+
+(defn upload-file [file]
+  (upload-file-to-path sounds-path file)
+  (redirect (str "/sound-files")))
--- src/clj/jenkinstein/routes/home.clj
+++ src/clj/jenkinstein/routes/home.clj
@@ -14,13 +14,20 @@
   (layout/render
     "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
 
-(defn about-page []
-  (layout/render "about.html"))
+(defn sound-files-page []
+  (layout/render "sound-files.html" (:body (files/list-files))))
+
+(defn sounds-page []
+  (layout/render "sound-entries.html" {:sounds (:body (sound/get-sounds))}))
 
 (defroutes home-routes
            (GET "/" [] (home-page))
-           (GET "/about" [] (about-page))
-           (GET "/list" [] (files/list-files))
+           (GET "/about" [] (layout/render "about.html"))
+           (GET "/upload" [] (layout/render "upload.html"))
+           (POST "/files" [file] (files/upload-file file))
+           (GET "/files" [] (files/list-files))
+           (GET "/sound-files" [] (sound-files-page))
+           (GET "/sound-entries" [] (sounds-page))
            (GET "/play/:file" [file] (playback/play file))
            (GET "/speak/:text" [text] (speech/speak text))
            (GET "/talk/:text" [text] (talk/talk text))
